0750600948368caa316f5a8e53845dff0b70bfee
Kev Field <kevfield@gmail.com>
2023-12-01 13:56:18 +0000

    changed grabbing first and last to a function

--- day01/main.go
+++ day01/main.go
@@ -13,19 +13,24 @@ func main() {
 	// pull in puzzle input
 	puzzleInput, _ := readFile(inputFile)
 
+	fmt.Println("Part 1 = ", getfirstLast(puzzleInput))
+}
+
+func getfirstLast(getfLinput []string) int {
 	combinestrings := ""
 	incCount := 0
 	totalCount := 0
 	var firstInt, lastInt string
-	for i := 0; i < len(puzzleInput); i++ {
+	for i := 0; i < len(getfLinput); i++ {
 		// filter out ints in the imported string
 		regFilter := regexp.MustCompile("[0-9]")
-		currentInt := (regFilter.FindAllString(puzzleInput[i], -1))
+		currentInt := (regFilter.FindAllString(getfLinput[i], -1))
 		firstInt = currentInt[0]
 		lastInt = currentInt[len(currentInt)-1]
 		combinestrings = fmt.Sprintf("%s%s", firstInt, lastInt)
 		incCount, _ = strconv.Atoi(combinestrings)
 		totalCount = totalCount + incCount
 	}
-	fmt.Println("Part 1 = ", totalCount)
+	return totalCount
+
 }
