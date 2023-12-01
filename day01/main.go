40126d20c7076bde32ad50ef9cd05a85fca58c02
Kev Field <kevfield@gmail.com>
2023-12-01 12:32:30 +0000

    completed day1 part1

--- day01/main.go
+++ day01/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -11,5 +13,19 @@ func main() {
 	// pull in puzzle input
 	puzzleInput, _ := readFile(inputFile)
 
-	fmt.Println(puzzleInput)
+	combinestrings := ""
+	incCount := 0
+	totalCount := 0
+	var firstInt, lastInt string
+	for i := 0; i < len(puzzleInput); i++ {
+		// filter out ints in the imported string
+		regFilter := regexp.MustCompile("[0-9]")
+		currentInt := (regFilter.FindAllString(puzzleInput[i], -1))
+		firstInt = currentInt[0]
+		lastInt = currentInt[len(currentInt)-1]
+		combinestrings = fmt.Sprintf("%s%s", firstInt, lastInt)
+		incCount, _ = strconv.Atoi(combinestrings)
+		totalCount = totalCount + incCount
+	}
+	fmt.Println("Part 1 = ", totalCount)
 }
